802ba321f47bb6d8e78765fa85aba9c497561105
Frank Cheng <ufo22940268@gmail.com>
2018-06-07 14:39:32 +0800

    tab and list

--- ImageCrawler/FirstViewController.swift
+++ ImageCrawler/FirstViewController.swift
@@ -7,19 +7,29 @@
 //
 
 import UIKit
+import Kingfisher
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! RequestImageCell
+        cell.image.kf.setImage(with: URL(string: "https://assets.materialup.com/uploads/a21b79fc-ec9b-4365-93aa-26dc28de0593/teaser.png"))
+        cell.layer.borderWidth = 10
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
---
+++ ImageCrawler/RequestImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  RequestImageCell.swift
+//  ImageCrawler
+//
+//  Created by Frank Cheng on 2018/6/7.
+//  Copyright © 2018 Frank Cheng. All rights reserved.
+//
+
+import UIKit
+
+class RequestImageCell: UICollectionViewCell {
+    
+    @IBOutlet weak var image: UIImageView!
+}
