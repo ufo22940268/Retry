8be0e6876cb145a602ebb42577d959e8cd955ea9
Frank Cheng <ufo22940268@gmail.com>
2018-06-07 09:57:05 +0800

    Initial Commit

---
+++ ImageCrawler/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ImageCrawler
+//
+//  Created by Frank Cheng on 2018/6/7.
+//  Copyright © 2018 Frank Cheng. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ImageCrawler/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ImageCrawler
+//
+//  Created by Frank Cheng on 2018/6/7.
+//  Copyright © 2018 Frank Cheng. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
