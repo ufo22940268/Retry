21930406ccfe9b1fa17832272714b2095971c19f
Frank Cheng <ufo22940268@gmail.com>
2018-06-21 11:24:22 +0800

    add navigation bar.

---
+++ ImageCrawler/HomeController.swift
@@ -0,0 +1,48 @@
+//
+//  HomeController.swift
+//  ImageCrawler
+//
+//  Created by Frank Cheng on 2018/6/21.
+//  Copyright © 2018 Frank Cheng. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class HomeController: UITabBarController {
+
+    @IBOutlet weak var navigationBar: UINavigationItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let item = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.refresh, target: self, action: #selector(HomeController.reset(sender:)))
+        navigationBar.rightBarButtonItem = item
+    }
+    
+    @IBAction func reset(sender: UINavigationItem) {
+        print("reset")
+        let realm = try! Realm()
+        try! realm.write {
+            realm.deleteAll()
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
